600bc8c1f89dfe2a920d23669f464dfdbe7aac30
arebruvold <arebru@gmail.com>
2024-01-22 22:27:42 +0100

    GAM and renamer fn.

--- _targets.R
+++ _targets.R
@@ -9,7 +9,7 @@ library(targets)
 
 # Set target options:
 tar_option_set(
-  packages = c("tibble", "ggmap") # packages that your targets need to run
+  packages = c("janitor", "tidyverse", "tibble", "ggmap", "readODS") # packages that your targets need to run
   # format = "qs", # Optionally set the default storage format. qs is fast.
   #
   # For distributed computing in tar_make(), supply a {crew} controller
@@ -44,16 +44,44 @@ options(clustermq.scheduler = "multicore")
 # Install packages {{future}}, {{future.callr}}, and {{future.batchtools}} to allow use_targets() to configure tar_make_future() options.
 
 # Run the R scripts in the R/ folder with your custom functions:
-tar_source()
+# tar_source()
 # source("other_functions.R") # Source other scripts as needed.
 
 # Replace the target list below with your own:
 list(
-tar_target(
-  stadia_map,
+  tar_target(
+    stadia_map,
     get_stadiamap(
       bbox = c(-10, 57.5, 50, 81),
       zoom = 5,
-      maptype = "alidade_smooth")
-)
+      maptype = "alidade_smooth"
+    )
+  ),
+  tar_target(
+    pfas_loqs,
+    read_ods("data/Kjemidatabasen_03_2023.ods", sheet = "ORGANIC") %>% clean_names() %>% select(
+      sample_id_o = sample_1,
+      year = cruise_3,
+      station,
+      core_id = sample_8,
+      sdepth_from = sample_interval_top_bottom,
+      sdepth_to = x10,
+      longitude = dde,
+      latitude = ddn,
+      depth = mbsl,
+      c(pfosa:pf_te_a)
+    ) %>%
+      # filter(if_any(all_of(pfases), ~.x != "nr")) %>%
+      slice(-1) %>%
+      mutate(across(matches("pf"), ~ if_else(str_detect(.x, "<"), str_replace(.x, "<", ""), NA) %>%
+        str_replace(",", ".") %>%
+        as.numeric())) %>%
+      select(sample_id_o, year, matches("pf")) %>%
+      pivot_longer(matches("pf"), names_to = "pfas", values_to = "loq") %>%
+      group_by(year, pfas) %>% distinct(loq) %>%
+      group_by(pfas) %>%
+      summarise(
+        max_loq = max(loq, na.rm = TRUE)
+      )
+  )
 )
